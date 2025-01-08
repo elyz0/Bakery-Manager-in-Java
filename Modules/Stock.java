5567161c016d67450600beeed7509501dae4fe57
elyz <elyzastudy@gmail.com>
2025-01-08 16:25:10 -0300

    third

--- Modules/Stock.java
+++ Modules/Stock.java
@@ -6,13 +6,7 @@ import java.util.Map;
 public class Stock {
     private Map<String, Items> items = new HashMap<>(); /* HashMap não garante a ordem das chaves mas tem melhor desempenho */
     private Map<String, Map<String, Integer>> recipes = new HashMap<>(); // Armazena as receitas  
-     
-    /*  public void addItem(String itemName, int quantity) {
-            items.put(item, items.getOrDefault(item, 0) + quantity); 
-        };  
-    Trabalha apenas com o nome e a quantidade do item, que são representados como String e Integer. Não há encapsulamento nem espaço para adicionar mais propriedades ao item no futuro (validade, descrição, categoria etc...).
-    */ 
-       
+    
     //Método para adicionar itens (ingredientes e produtos) ao estoque  
     public void addItem(String itemName, int quantity) {
         Items item = items.getOrDefault(itemName, new Items(itemName, 0)); //método da interface Map que retorna o valor associado a uma chave específica. Se a chave não existir no mapa, retorna um valor padrão fornecido como argumento.
@@ -20,14 +14,6 @@ public class Stock {
         items.put(itemName, item); //atualiza o mapa como o objeto Item
     }
     
-    /*public void removeItem(String item, int quantity) {
-        if (items.containsKey(item) && items.get(item) >= quantity) {
-            items.put(item, items.get(item) - quantity);
-        } else {
-            System.out.println("Estoque insuficiente ou item inexistente: " + item);
-        }
-    }*/ 
-     
     //Método para remover itens do estoquec
     public void removeItem(String itemName, int quantity) {
         Items item = items.get(itemName);
@@ -38,7 +24,6 @@ public class Stock {
         }
     }
 
-      
     //Método para verificar a quantidade em estoque de um item
     public int checkStock(String itemName) { 
         Items item = items.get(itemName); //Get busca o objeto associado ao nome
